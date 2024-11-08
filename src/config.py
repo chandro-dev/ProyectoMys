4e0399b7292c61b4586931dff0cd74408292e1b6
Aejandro <alejocarreteroballesteros@gmail.com>
2024-11-08 09:17:34 -0500

    add .env

--- src/config.py
+++ src/config.py
@@ -1,8 +1,17 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
+from dotenv import load_dotenv
 
-DATABASE_URL = "postgresql://yourusername:yourpassword@192.168.1.58:5432/CRM"
+# Cargar variables desde el archivo .env
+load_dotenv()
+db_host = os.getenv("DB_HOST")
+db_user = os.getenv("DB_USER")
+db_pass = os.getenv("DB_PASS")
+DATABASE_URL = f"postgresql://{db_user}:{db_pass}@{db_host}/CRM"
+
+print(DATABASE_URL)
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
