6fb43d48c4ba7e8abf099af0ce62bf54a872a38e
Jose Larrada <jalarrada@unicesar.edu.co>
2024-11-16 18:35:16 -0500

    Ultimos cambios

--- src/controllers/sells_controller.py
+++ src/controllers/sells_controller.py
@@ -11,3 +11,15 @@ def save_sell(sell:SalesOpportunitySchema = Body()):
 @router.get("/getSales/{stage}",tags=['Oportunidades de ventas'])
 def getSalesOportunity(stage:int=Path):
     return get_sales_opportunities_by_stage(stage)
+
+@router.get("/getAll",tags=['Oportunidades de ventas'])
+def getAllsalesOportunity():
+    return get_all_sales_opportunities()
+
+@router.delete("/deleteOportunity/{id}",tags=['Oportunidades de ventas'])
+def deleteSalesOportunity(id:str=Path):
+    return delete_sales_opportunity(id)
+
+@router.put("/updateStatus/{idOportunity}/{status}",tags=['Oportunidades de ventas'])
+def updateStatus(idOportunity:str=Path,status:str=Path):
+    return update_sales_opportunity_status(idOportunity,status)
